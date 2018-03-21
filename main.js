5caddc8e943059e6959c743354f5ef5dc6350dc8
xielinxu <799104846@qq.com>
2018-03-21 11:31:40 +0800

    first_blood2

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 930, height: 700 })
+  win = new BrowserWindow({ width: 930, height: 700,icon: path.join(__dirname, 'voc.ico') })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
